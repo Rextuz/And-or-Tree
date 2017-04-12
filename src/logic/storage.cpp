bd94c577a463d1d582311e012e9bc3b3186e2e26
Max Kanushin <rextuz@gmail.com>
2017-04-12 11:36:16 +0300

    Storage: fixed warning.

--- src/logic/storage.cpp
+++ src/logic/storage.cpp
@@ -28,7 +28,7 @@ void Storage::loadList()
     }
 
     std::cout << "Templates:" << std::endl;
-    for(int i = 0; i < templates.size(); ++i) {
+    for(unsigned int i = 0; i < templates.size(); ++i) {
         qDebug() << templates[i];
     }
 }
